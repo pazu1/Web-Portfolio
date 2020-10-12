30050864405f741aa9ee0b56799492f7f2d4a225
pazu1 <pazu1s@outlook.com>
2020-10-12 19:30:39 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/App.scss";
+
+function App() {
+    return (
+        <div className="appRoot">
+            <div className="headerContainer">
+                <h1>FName LName</h1>
+                <h2>Full-Stack Developer</h2>
+            </div>
+
+            <div className="appContent-main">
+                <strong>...void...</strong>
+            </div>
+        </div>
+    );
+}
+
+export default App;
